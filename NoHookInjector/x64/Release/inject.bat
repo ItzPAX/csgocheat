20d93366df0fd0e03d798d075fa7203ead96394b
Emlin <49819627+R4YVEN@users.noreply.github.com>
2021-12-29 22:42:00 +0100

    Added RayInj

---
+++ NoHookInjector/x64/Release/inject.bat
@@ -0,0 +1,10 @@
+@echo off
+cd C:\Users\emlin\source\repos\NoHookInjector\x64\Release
+echo RayInjector - the syscalling and hook-defeating dll-injector
+echo.
+set /p process=Process name: 
+set /p dllpath=Dll (full path/relative path): 
+echo.
+RayInj.exe "%process%" "%dllpath%"
+echo.
+pause
